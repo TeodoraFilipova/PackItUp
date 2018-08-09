e71bf73de674b92e99bb74809dbda92370e9375f
TeodoraFilipova <t.filipova13@gmail.com>
2018-08-09 18:00:58 +0300

    Implemented drawer navigation (abstract class) in ItemListActivity

--- app/src/main/java/com/example/vision_pc3/packitup/ItemList/ItemListActivity.java
+++ app/src/main/java/com/example/vision_pc3/packitup/ItemList/ItemListActivity.java
@@ -3,17 +3,23 @@ package com.example.vision_pc3.packitup.ItemList;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.vision_pc3.packitup.R;
+import com.example.vision_pc3.packitup.utilities.BaseDrawerActivity;
 
-public class ItemListActivity extends AppCompatActivity {
-
+public class ItemListActivity extends BaseDrawerActivity {
+    public static final long ID = 3;
     private ItemListFragment mItemListFragment;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
+
+        mToolbar = findViewById(R.id.drawer_toolbar);
+        setSupportActionBar(mToolbar);
 
         Intent intent = getIntent();
         String category = intent.getStringExtra("CATEGORY_NAME");
@@ -24,5 +30,17 @@ public class ItemListActivity extends AppCompatActivity {
         getFragmentManager().beginTransaction()
                 .replace(R.id.content, mItemListFragment)
                 .commit();
+
+        setUpDrawer();
+    }
+
+    @Override
+    protected long getId() {
+        return ID;
+    }
+
+    @Override
+    protected Toolbar getDrawerToolbar() {
+        return mToolbar;
     }
 }
