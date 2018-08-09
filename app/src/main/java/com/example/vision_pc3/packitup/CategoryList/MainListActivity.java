d08b039002ef6f02f161fcbe25a4197167900e01
TeodoraFilipova <t.filipova13@gmail.com>
2018-08-09 13:18:44 +0300

    Updated package names to reflect change in project design

--- app/src/main/java/com/example/vision_pc3/packitup/CategoryList/MainListActivity.java
+++ app/src/main/java/com/example/vision_pc3/packitup/CategoryList/MainListActivity.java
@@ -1,11 +1,14 @@
-package com.example.vision_pc3.packitup.ItemList;
+package com.example.vision_pc3.packitup.CategoryList;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.vision_pc3.packitup.ItemList.ItemListActivity;
 import com.example.vision_pc3.packitup.R;
+import com.example.vision_pc3.packitup.utilities.Navigator;
 
-public class MainListActivity extends AppCompatActivity {
+public class MainListActivity extends AppCompatActivity implements Navigator {
 
     private MainListFragment mMainListFragment;
 
@@ -15,10 +18,18 @@ public class MainListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_list);
 
         mMainListFragment = MainListFragment.getInstance();
+        mMainListFragment.setNavigator(this);
 
         getFragmentManager()
                 .beginTransaction()
                 .replace(R.id.content, mMainListFragment)
                 .commit();
+    }
+
+    @Override
+    public void navigateTo(String category) {
+        Intent intent = new Intent(this, ItemListActivity.class);
+        intent.putExtra("CATEGORY_NAME", category);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/vision_pc3/packitup/CategoryList/MainListFragment.java
@@ -0,0 +1,80 @@
+package com.example.vision_pc3.packitup.CategoryList;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.vision_pc3.packitup.models.PackingItem;
+import com.example.vision_pc3.packitup.R;
+import com.example.vision_pc3.packitup.utilities.Navigator;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainListFragment extends Fragment implements AdapterView.OnItemClickListener {
+
+    private FirebaseFirestore mDb;
+    private ListView mCategoriesListView;
+    private ArrayAdapter<String> mCategoriesAdapter;
+    private Navigator mNavigator;
+
+    public MainListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_list, container, false);
+
+        mDb = FirebaseFirestore.getInstance();
+
+        mCategoriesListView = view.findViewById(R.id.lv_items);
+        mCategoriesAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1);
+        mCategoriesListView.setAdapter(mCategoriesAdapter);
+        mCategoriesListView.setOnItemClickListener(this);
+
+        mDb.collection("packingitems")
+                .get()
+                .addOnCompleteListener(task -> {
+                    List<PackingItem> packingItems = task.getResult().toObjects(PackingItem.class);
+                    Set<String> categories = new HashSet<>();
+                    for (PackingItem item : packingItems) {
+                        categories.add(item.getCategory());
+                    }
+                    mCategoriesAdapter.addAll(categories);
+                });
+
+        return view;
+    }
+
+    public static MainListFragment getInstance() {
+        return new MainListFragment();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        String category = mCategoriesAdapter.getItem(i);
+        mNavigator.navigateTo(category);
+    }
+
+    public void setNavigator(Navigator navigator) {
+        this.mNavigator = navigator;
+    }
+}
