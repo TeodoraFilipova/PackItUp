2f18e67342d09e1a09ab4901ab249aed97d5cab9
TeodoraFilipova <t.filipova13@gmail.com>
2018-08-10 12:38:49 +0300

    Separated repository access in a class (implementing interface) to minimize repeating code and increase reusability

---
+++ app/src/main/java/com/example/vision_pc3/packitup/base/FirebaseRepository.java
@@ -0,0 +1,40 @@
+package com.example.vision_pc3.packitup.base;
+
+import com.example.vision_pc3.packitup.models.PackingItem;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+
+public class FirebaseRepository<T> implements Repository<T> {
+    private final FirebaseFirestore mDb;
+    private final Class<T> mKlass;
+    private final String mCollectionName;
+
+    public FirebaseRepository(Class<T> klass) {
+        mDb = FirebaseFirestore.getInstance();
+        mKlass = klass;
+        mCollectionName = mKlass.getSimpleName().toLowerCase() + "s";
+    }
+
+    @Override
+    public void getAll(Consumer<List<T>> action) {
+        mDb.collection(mCollectionName)
+                .get()
+                .addOnCompleteListener(task -> {
+                    List<T> items = task.getResult().toObjects(mKlass);
+                    action.accept(items);
+                });
+    }
+
+    @Override
+    public void add(T item, Consumer<T> action) {
+        mDb.collection(mCollectionName)
+                .add(item)
+                .addOnCompleteListener(task -> {
+                    action.accept(item);
+                });
+    }
+}
---
+++ app/src/main/java/com/example/vision_pc3/packitup/base/Repository.java
@@ -0,0 +1,11 @@
+package com.example.vision_pc3.packitup.base;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public interface Repository<T> {
+
+    void getAll(Consumer<List<T>> action);
+
+    void add(T item, Consumer<T> action);
+}
